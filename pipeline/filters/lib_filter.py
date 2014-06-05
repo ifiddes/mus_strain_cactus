478e9ad5d3cfca031ef50629e124b810dfe29587
Dent Earl <dearl@soe.ucsc.edu>
2014-06-05 14:30:47 -0700

    added functions to get sequences from a fasta

--- pipeline/filters/lib_filter.py
+++ pipeline/filters/lib_filter.py
@@ -3,6 +3,21 @@ convenience library for assisting filters.
 """
 import os
 
+class Sequence(object):
+  """ Represents a sequence of DNA.
+  """
+  def __init__(self, name):
+    self.name = name  # chromosome or scaffold name
+    self._sequence = ''  # ACGTs
+  def addSequence(self, seq):
+    self._sequence += seq
+  def setSequence(self, seq):
+    self._sequence = seq
+  def getSequence(self):
+    return self._sequence
+  def getLength(self):
+    return len(self._sequence)
+
 
 def InitializeArguments(parser):
   parser.add_argument('--refGenome')
@@ -44,3 +59,43 @@ def CheckArguments(args, parser):
   # directory
   if not os.path.isdir(args.outDir):
     parser.error('--outDir=%s is not a directory' % args.outDir)
+
+
+def getSequences(seqFile):
+  """ Given a path to a fasta file, return a dictionary of Sequence objects
+  keyed on the sequence name
+  """
+  seqDict = {}
+  seq = None
+  with open(seqFile, 'r') as f:
+    for line in f:
+      line = line.strip()
+      if line == '':
+        continue
+      if line.startswith('>'):
+        name = line.replace('>', '').strip()
+        seq = Sequence(name)
+        seqDict[name] = seq
+        continue
+      seq.addSequence(line)
+  return seqDict
+
+def getSequence(seqFile, seqName):
+  """ Given a path to a fasta file and a name, return a sequence object.
+  """
+  seq = Name
+  with open(seqFile, 'r') as f:
+    for line in f:
+      line = line.strip()
+      if line == '':
+        continue
+      if line.startswith('>'):
+        if seq is not None:
+          return seq
+        name = line.replace('>', '').strip()
+        if seqName == name:
+          seq = Sequence(name)
+        continue
+      if seq is not None:
+        seq.addSequence(line)
+  return seq
