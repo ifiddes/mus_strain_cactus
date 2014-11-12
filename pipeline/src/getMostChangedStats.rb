0e8787290cfba803896f4eb1f47399ffb519bbdf
Joel Armstrong <jcarmstr@ucsc.edu>
2014-11-12 06:33:36 -0800

    fix highest-fold-change reporting to work when one file has tag below threshold

--- pipeline/src/getMostChangedStats.rb
+++ pipeline/src/getMostChangedStats.rb
@@ -1,9 +1,11 @@
 #!/usr/bin/env ruby
+require 'set'
 require 'nokogiri'
 
 (warn "Usage: #{$0} statsXML1 statsXML2"; exit(1)) if ARGV.size != 2
 
-MIN_TRANSCRIPT_FRAC=0.02
+MIN_TRANSCRIPT_FRAC=0.02 # Minimum % of transcripts that a tag must
+                         # apply to for it to be reported
 def get_transcripts_per_tag(xml)
   h = {}
   xml.traverse { |node| h[node.path] = node["transcripts"].to_i }
@@ -23,13 +25,16 @@ stats2 = Nokogiri::XML(open(ARGV[1]))
 transcripts_per_tag_1 = get_transcripts_per_tag(stats1)
 transcripts_per_tag_2 = get_transcripts_per_tag(stats2)
 
-transcript_frac_per_tag_1 = normalize(transcripts_per_tag_1).select { |k, v| v >= MIN_TRANSCRIPT_FRAC }
-transcript_frac_per_tag_2 = normalize(transcripts_per_tag_2).select { |k, v| v >= MIN_TRANSCRIPT_FRAC }
+transcript_frac_per_tag_1 = normalize(transcripts_per_tag_1)
+transcript_frac_per_tag_2 = normalize(transcripts_per_tag_2)
+
+significant_tags = Set.new(transcript_frac_per_tag_1.keys).union(Set.new(transcript_frac_per_tag_2.keys)).select { |k| (transcript_frac_per_tag_1[k] || 0) >= MIN_TRANSCRIPT_FRAC || (transcript_frac_per_tag_2[k] || 0) >= MIN_TRANSCRIPT_FRAC }
 
 puts "tag\tfoldChange\ttranscripts1\ttranscripts2\ttranscriptFrac1\ttranscriptFrac2"
 foldChanges = {}
 transcript_frac_per_tag_1.each { |k, v| foldChanges[k] = transcript_frac_per_tag_2[k] ? transcript_frac_per_tag_2[k]/v : 0 }
 transcript_frac_per_tag_2.each { |k, v| foldChanges[k] = Float::INFINITY unless foldChanges[k] }
+foldChanges.keep_if {|k, v| significant_tags.include?(k) }
 foldChanges.to_a.sort {|e1, e2| e1[1] <=> e2[1]}.each do |e|
   tag, foldChange = e
   puts "#{tag}\t#{foldChange}\t#{transcripts_per_tag_1[tag]}\t#{transcripts_per_tag_2[tag]}\t#{transcript_frac_per_tag_1[tag]}\t#{transcript_frac_per_tag_2[tag]}"
